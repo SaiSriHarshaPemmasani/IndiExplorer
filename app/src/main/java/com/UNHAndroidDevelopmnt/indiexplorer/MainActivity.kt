44b3e9b57445e7ebc8000db39568a734f4cddcbe
SaiSriHarshaPemmasani <spemm2@unh.newhaven.edu>
2023-11-12 21:42:01 -0500

    pushed the project

---
+++ app/src/main/java/com/UNHAndroidDevelopmnt/indiexplorer/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.UNHAndroidDevelopmnt.indiexplorer
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kotlinx.coroutines.delay
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //my changes for code
+        //setting notch or status to transparent
+        window.statusBarColor=Color.TRANSPARENT
+
+        //creating a handler
+        //handler to change screen to signup page in 3 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this,SignUpActivity::class.java))
+                },3000)
+
+    }
+}
