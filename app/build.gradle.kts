3c8f7ab64614cad44b525d88d3d5ce8800ad0983
SaiSriHarshaPemmasani <spemm2@unh.newhaven.edu>
2023-11-13 00:48:54 -0500

    Connected SignUp page to the Firebase Server

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.UNHAndroidDevelopmnt.indiexplorer"
     compileSdk = 34
+
+
+    buildFeatures{
+        viewBinding=true
+    }
 
     defaultConfig {
         applicationId = "com.UNHAndroidDevelopmnt.indiexplorer"
@@ -41,6 +47,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -50,4 +57,7 @@ dependencies {
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
     implementation("com.android.support:multidex:1.0.3")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- app/src/main/java/com/UNHAndroidDevelopmnt/indiexplorer/SignUpActivity.kt
+++ app/src/main/java/com/UNHAndroidDevelopmnt/indiexplorer/SignUpActivity.kt
@@ -2,10 +2,56 @@ package com.UNHAndroidDevelopmnt.indiexplorer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.UNHAndroidDevelopmnt.indiexplorer.Models.User
+import com.UNHAndroidDevelopmnt.indiexplorer.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+    val binding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
     }
+    lateinit var user: User
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(binding.root)
+            user = User()
+
+            binding.SignUpBtn.setOnClickListener{
+                if ( binding.UserName.editText?.text.toString().equals("") or
+                    binding.UserEmail.editText?.text.toString().equals("") or
+                    binding.UserPassword.editText?.text.toString().equals("") )
+                {
+                    Toast.makeText(this@SignUpActivity,"Please fill all the required fields",Toast.LENGTH_LONG).show()
+                }else{
+
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                        binding.UserEmail.editText?.text.toString(),
+                        binding.UserPassword.editText?.text.toString()
+                    ).addOnCompleteListener {
+                        result->
+
+                        if (result.isSuccessful){
+                            user.name=binding.UserName.editText?.text.toString()
+                            user.email=binding.UserEmail.editText?.text.toString()
+                            user.password=binding.UserPassword.editText?.text.toString()
+                            Firebase.firestore.collection("User")
+                                .document(Firebase.auth.currentUser!!.uid).set(user)
+                                .addOnSuccessListener {
+                                    Toast.makeText(this@SignUpActivity,"Login Successful ",Toast.LENGTH_LONG).show()
+                                }
+
+                        }else{
+                            Toast.makeText(this@SignUpActivity,result.exception?.localizedMessage,Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                }
+            }
+        }
 }
